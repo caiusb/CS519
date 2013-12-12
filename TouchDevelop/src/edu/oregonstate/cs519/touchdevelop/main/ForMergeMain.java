26936e0a177f465a1791783f152eec41e6b42dc7
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-12-11 16:19:00 -0800

    Changes to the main

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/ForMergeMain.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/ForMergeMain.java
@@ -13,10 +13,13 @@ public class ForMergeMain {
 		String json2 = new String(Files.readAllBytes(Paths.get("../microedits/sum/v02.json")));
 		
 		History history = new History(json1, "a");
-		ASTNode program = history.apply();
-		History history2 = new History(program, json2, "b");
-		ASTNode program2 = history2.apply();
-		System.out.println(program2.toJSONString());
+		History history2 = new History(json2, "b");
+		history2.getProgram().matchWith(history.getProgram());
+		ASTNode afterFirstApplication = history.apply();
+		history2 = new History(afterFirstApplication, json2, "b");
+		ASTNode finalMerge = history2.apply();
+		
+		System.out.println(finalMerge.getJSON());
 		System.out.println("There were " + history2.getConflicts().size() + " conflicts");
 	}
 
